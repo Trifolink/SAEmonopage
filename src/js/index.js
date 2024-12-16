e56a9b3e5a5ba1f4c16c43dfd095201842b56129
Novais <abigail62810@gmail.com>
2024-12-16 14:33:33 +0100

    vinyle

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 ScrollReveal().reveal('.headline');
+
+// Animation d'entrée initiale (descend du haut)
+ScrollReveal().reveal('.titre', {
+    distance: '50px',   // Déplacement vertical
+    origin: 'top',      // Vient du haut
+    duration: 1000,     // Durée de l'entrée
+    delay: 300,         // Délai avant l'animation
+    easing: 'ease-out', // Transition fluide
+    reset: true         // L'animation se rejoue si on remonte
+});
+
+// Animation de sortie (remonte et disparaît au scroll)
+
+
