73c05bf05370c5a41290efa6adccc4e7dc642aae
Nathan COQUART <coquartnathan@gmail.com>
2024-12-16 14:11:08 +0100

    h1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 ScrollReveal().reveal('.headline');
+
+ScrollReveal().reveal('.titre', {
+    // distance: '400px',
+    distance: '350%',
+    opacity: 1,
+    duration: 0,
+    reset: true,
+    scale: 1,
+    easing: 'ease-out',
+    beforeReveal: function (el) {
+        el.style.position = 'fixed';
+        el.style.left = '50%';
+        el.style.transform = 'translateX(-50%)';
+    }
+
+});
